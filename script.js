1278197dae0d5abeff8bfeae4c872d542bbc9cbb
NatCanCode <nathalie.piat1@gmail.com>
2022-10-13 09:29:48 +0200

    first and only commit cause super simple project

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+// With strict mode you get more useful errors when you make mistakes
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+});
